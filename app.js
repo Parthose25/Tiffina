4a000f70ce0d2c3357830b7ff9f1a8a74b87c372
Parthose25 <84158326+Parthose25@users.noreply.github.com>
2021-05-21 03:35:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+//sidenav
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+  
+  //carousel
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+  //scrollspy
+
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+
+  //materialbox
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
+
+  //datepicker
+  
+  $('.datepicker').datepicker({
+    disableWeekends: true,
+    yearRange: 1
+  });
+
